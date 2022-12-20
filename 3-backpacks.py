c6553728204d514883b8514aff591e181f4019f6
Igor Wawrzyniak <igorwaw@gmail.com>
2022-12-20 13:27:13 +0100

    day 3 reorganized - move both parts to 1 file

--- 3-backpacks.py
+++ 3-backpacks.py
@@ -12,6 +12,19 @@ def get_next_pair(filename):
             yield set(leftpart),set(rightpart)
         
 
+def get_next_group(filename, length):
+    with open(filename) as inputfile:
+        while True:
+            group = []
+            for i in range(length):
+                try:
+                    group.append(set(next(inputfile).strip()))
+                except StopIteration:
+                    return
+                if not group:
+                    return
+            yield group
+
 
 def calculate_priority(item):
     if 'a'<=item<='z':
@@ -22,7 +35,7 @@ def calculate_priority(item):
 
 
     
-
+#part 1
 elfcount=0
 priosum=0
 for leftset,rightset in get_next_pair(INPUTFILE):
@@ -33,5 +46,18 @@ for leftset,rightset in get_next_pair(INPUTFILE):
     priosum+=priority
     #print("Elf:", elfcount, "left:", leftset, "right:", rightset, "common: ", commonchar, "priority: ", priority)
 
-print("--------------------------")
 print("Number of elves: ", elfcount, "Total prio: ", priosum)
+
+groupcount=0
+priosum=0
+
+#part 2
+for elf1,elf2,elf3 in get_next_group(INPUTFILE,3):
+    groupcount+=1
+    commonset = elf1 & elf2 & elf3
+    commonchar=next(iter(commonset)) # ugly way to extract element from 1-element set
+    priority=calculate_priority(commonchar)
+    priosum+=priority
+    #print("Group:", groupcount, "common: ", commonchar, "priority: ", priority)
+
+print("Number of groups: ", groupcount, "Total prio: ", priosum)
